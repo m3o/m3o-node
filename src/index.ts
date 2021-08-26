d4f0ddb7c14aad31417e29e5fd3f8b825c20017e
Martin Virando <martinvirando@hotmail.co.uk>
2021-08-26 11:52:41 +0100

    Added the fix for require and es6 import

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
-import {Client} from './client';
 import {Stream} from './stream';
+import {Client} from './client';
+import * as user from './user';
+import * as db from './db';
 
-export * from './user';
-export * from './db';
+// export * from './user';
+// export * from './db';
 
 export default {
   Client,
   Stream,
+  ...user,
+  ...db,
+};
+
+module.exports = {
+  Client,
+  Stream,
+  ...user,
+  ...db,
 };
